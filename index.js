3b6637140800364a9eb2604e358796c3a6b03ffa
Talentos C08100N <UG_TALENTOS_C08100N@asav.brm>
2023-08-30 16:07:10 -0300

    primeiro commit

---
+++ index.js
@@ -0,0 +1,257 @@
+//
+
+const pokedex = [
+    {
+        indice: "#0001",
+        pokemon: "bulbasauro",
+        tipo: ["planta","veneno"]
+
+    },
+    {
+        indice: "#0002",
+        pokemon: "ivysauro",
+        tipo: ["planta","veneno"]
+
+    },
+    {
+        indice: "#0003",
+        pokemon: "venosauro",
+        tipo: ["planta","veneno"]
+
+    },
+    {
+        indice: "#0004",
+        pokemon: "charmander",
+        tipo: ["fogo"]
+
+    },
+    {
+        indice: "#0005",
+        pokemon: "charmeleon",
+        tipo: ["fogo"]
+
+    },
+    {
+        indice: "#0006",
+        pokemon: "charizard",
+        tipo: ["fogo","voador"]
+
+    },
+    {
+        indice: "#0007",
+        pokemon: "squirtle",
+        tipo: ["agua"]
+
+    },
+    {
+        indice: "#0008",
+        pokemon: "warturtle",
+        tipo: ["agua"]
+
+    },
+    {
+        indice: "#0009",
+        pokemon: "blastoise",
+        tipo: ["agua"]
+
+    },
+    {
+        indice: "#0010",
+        pokemon: "caterpie",
+        tipo: ["inceto"]
+
+    },
+    {
+        indice: "#0011",
+        pokemon: "metapod",
+        tipo: ["inceto"]
+
+    },
+    {
+        indice: "#0012",
+        pokemon: "butterfly",
+        tipo: ["inceto","voador"]
+
+    },
+    {
+        indice: "#0013",
+        pokemon: "weedle",
+        tipo: ["inceto","veneno"]
+
+    },
+    {
+        indice: "#0014",
+        pokemon: "kakuna",
+        tipo: ["inceto","veneno"]
+
+    },
+    {
+        indice: "#0015",
+        pokemon: "beedrill",
+        tipo: ["inceto","veneno"]
+
+    },
+    {
+        indice: "#0016",
+        pokemon: "pigdey",
+        tipo: ["normal","voador"]
+
+    },
+    {
+        indice: "#0017",
+        pokemon: "pigdeotto",
+        tipo: ["inceto","voador"]
+
+    },
+    {
+        indice: "#0018",
+        pokemon: "pigdeot",
+        tipo: ["inceto","voador"]
+
+    },
+    {
+        indice: "#0019",
+        pokemon: ["ratata","ratata de alola"],
+        tipoRatata: ["normal"],
+        tipoRatataDeAlola: ["normal","dark"]
+
+    },
+    {
+        indice: "#0020",
+        pokemon: ["raticate","raticate de alola"],
+        tipoRaticate: ["normal"],
+        tipoRaticateDeAlola: ["normal","dark"]
+    },
+    {
+        indice: "#0021",
+        pokemon: "spearow",
+        tipo: ["normal","voador"]
+    },
+    {
+        indice: "#0022",
+        pokemon: "fearow",
+        tipo: ["normal","voador"]
+    },
+    {
+        indice: "#0023",
+        pokemon: "ekans",
+        tipo: ["veneno"]
+    },
+    {
+        indice: "#0024",
+        pokemon: "arbok",
+        tipo: ["veneno"]
+    },
+    {
+        indice: "#0025",
+        pokemon: "pikacho",
+        tipo: ["eletrico"]
+    },
+    {
+        indice: "#0026",
+        pokemon: ["raichu","raichu De alola"],
+        tipoRaichu: ["eletrico"],
+        tipoRaichuDeAlola: ["eletrico","psiquico"]
+    },
+    {
+        indice: "#0027",
+        pokemon: ["sandshrew","sandshrew De alola"],
+        tiposnadshrew: ["terra"],
+        tiposnadshrewDeAlola: ["gelo","metal"]
+    },
+    {
+        indice: "#0028",
+        pokemon: ["sandslash","sandslash De alola"],
+        tiposandslash: ["terra"],
+        tiposandslashDeAlola: ["gelo","metal"]
+    },
+    {
+        indice: "#0029",
+        pokemon: "nidoran",
+        tipo: ["veneno"]
+    },
+    {
+        indice: "#0030",
+        pokemon: "nidorina",
+        tipo: ["veneno"]
+    },
+    {
+        indice: "#0031",
+        pokemon: "nidoqueen",
+        tipo: ["veneno","terra"]
+    },
+    {
+        indice: "#0032",
+        pokemon: "nidoran",
+        tipo: ["veneno"]
+    },
+    {
+        indice: "#0033",
+        pokemon: "nidorino",
+        tipo: ["veneno"]
+    },
+    {
+        indice: "#0034",
+        pokemon: "nidoking",
+        tipo: ["veneno","terra"]
+    },
+    {
+        indice: "#0035",
+        pokemon: "clefairy",
+        tipo: ["fada"]
+    },
+    {
+        indice: "#0036",
+        pokemon: "clefable",
+        tipo: ["fada"]
+    },
+    {
+        indice: "#0037",
+        pokemon: ["vulpix","vulpix De alola"],
+        tipovulpix: ["fogo"],
+        tipovulpixDeAlola: ["gelo"]
+    },
+    {
+        indice: "#0038",
+        pokemon: ["ninetales","ninetales De alola"],
+        tiponinetales: ["fogo"],
+        tiponinetalesDeAlola: ["gelo","fada"]
+    },
+    {
+        indice: "#0039",
+        pokemon: "jigglypuff",
+        tipo: ["fada"]
+    },
+    {
+        indice: "#0040",
+        pokemon: "Wigglytuff",
+        tipo: ["fada"]
+    },
+    {
+        indice: "#0041",
+        pokemon: "zubat",
+        tipo: ["veneno","voador"]
+    },
+    {
+        indice: "#0042",
+        pokemon: "golbat",
+        tipo: ["veneno","voador"]
+    },
+    {
+        indice: "#0043",
+        pokemon: "oddish",
+        tipo: ["grama","veneno"]
+    },
+    {
+        indice: "#0044",
+        pokemon: "gloom",
+        tipo: ["grama","veneno"]
+    },
+    {
+        indice: "#0045",
+        pokemon: "vileplume",
+        tipo: ["grama","veneno"]
+    }
+]
+
+console.log(pokedex)
